766f08dbf144317970160b18910fb2db2b3e9f59
Haydon Pool <hpool2004@gmail.com>
2023-08-11 04:12:37 -0500

    Completed problem Multiply

---
+++ 8kyu/Multiply.cpp
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/50654ddff44f800200000004
+
+//This is a test for workflow. The problem itself is pretty self explanitory
+int multiply(int a, int b)
+{
+    return a * b;
+}
